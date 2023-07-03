18fd77a34c86273f6c05105c0f98b239f8b20ca3
msko <41417515+dkandkanehd@users.noreply.github.com>
2023-07-03 09:53:50 +0900

    2023.07.03

--- practice4/src/App.tsx
+++ practice4/src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import styled from "styled-components";
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-  `;
-
+import React, {useState} from "react";
 
 function App() {
+
+  const [value, setValue] = useState("");
+
+  const onChange = (event : React.FormEvent<HTMLInputElement>) => {
+      const {
+        currentTarget: {value},
+      } = event;
+      setValue(value);
+  };
+
+  const onSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
-    <Wrapper>
-        <Title>Hello</Title>
-    </Wrapper>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input 
+          value = {value}
+          onChange={onChange}
+          type="text" 
+          placeholder="username" 
+        />
+        <button>Log in</button>
+      </form>
+    </div>
   );
 }
 
